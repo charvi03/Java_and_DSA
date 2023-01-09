6a91de4270eaca35c2e2407cc529a0e92b508884
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-09 22:08:20 +0530

    ini

---
+++ src/data_types.java
@@ -0,0 +1,17 @@
+public class data_types {
+    public static void main(String[] args) {
+        byte b = 9; //
+        System.out.println(b);
+        char ch = 'a'; //single character
+        System.out.println(ch);
+        boolean var = false; //true or false
+        System.out.println(var);
+        float price = 10.5f; //decimal values
+        System.out.println(price);
+        int number = 25; //large whole number
+        System.out.println(number);
+        //  long - long integer number
+        //double -- long decimal number
+        // short-- store any number
+    }
+}
