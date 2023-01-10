05b5308df2f9650beaaca4c8a76dc2d811f0bf5b
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-10 19:22:26 +0530

    init

--- src/inputs.java
+++ src/inputs.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class inputs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String inp = scanner.next();
+        //String inp = scanner.next();
+       // String inp1=scanner.nextLine();
+        int num=scanner.nextInt();
+        System.out.println(num);
 
     }
 }
--- src/sum.java
+++ src/sum.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class sum {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+
+        //int a = 10;
+        // int b = 5;
         int sum = a + b;
-        System.out.println(sum);
+        System.out.println("The sum is " + sum);
     }
 }
