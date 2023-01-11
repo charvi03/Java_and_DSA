c6750daeae76ad2bef9279503feae86a96691d20
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-11 21:38:09 +0530

    init

---
+++ src/Operators/assigment.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class assigment {
+    public static void main(String[] args) {
+        int a = 50;
+        a += 10;
+        System.out.println(a);
+        int b = 50;
+        b -= 10;
+        System.out.println(b);
+        int c = 50;
+        c *= 10;
+        System.out.println(c);
+        int d = 50;
+        d /= 10;
+        System.out.println(d);
+    }
+}
---
+++ src/Operators/logical.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class logical {
+    public static void main(String[] args) {
+        System.out.println((3 > 2) && (5 > 0));
+        System.out.println((3 > 2) && (5 < 0));
+        System.out.println((3 > 2) || (5 < 0));
+        System.out.println(!(3 > 2));
+    }
+}
