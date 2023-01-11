a056c9ee393473fd7242989943ce72f6c4f7f421
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-11 21:22:49 +0530

    init

---
+++ src/Operators/relational.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class relational {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 10;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a == c);
+        System.out.println(a > b);
+        System.out.println(a > c);
+        System.out.println(a < b);
+        System.out.println(a >= c);
+        System.out.println(a >= b);
+
+    }
+}
