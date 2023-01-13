d05056eaad47d490ea7d6b2f0bb79f82c423ef3b
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-13 22:09:15 +0530

    init

--- src/Conditionals/ifElse.java
+++ src/Conditionals/ifElse.java
@@ -2,14 +2,12 @@ package Conditionals;
 
 public class ifElse {
     public static void main(String[] args) {
-        int age = 22;
+        int age = 77;
         if (age >= 18) {
             System.out.println("adult: drive,vote");
-        }
-//        if (age > 13 && age < 18) {
-//            System.out.println("teenage");
-//        }
-        else {
+        } else if (age > 13 && age < 18) {
+            System.out.println("teenage");
+        } else {
             System.out.println("not adult");
         }
     }
