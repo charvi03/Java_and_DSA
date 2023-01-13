57940e1e686635adc2174f5c03abb2d4a69ca814
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-13 22:08:18 +0530

    init

--- src/Conditionals/largest_of_three.java
+++ src/Conditionals/largest_of_three.java
@@ -1,4 +1,16 @@
 package Conditionals;
 
 public class largest_of_three {
+    public static void main(String[] args) {
+        int a = 1, b = 3, c = 6;
+
+        if ((a >= b) && (a >= c)) {
+            System.out.println("A is largest");
+        } else if (b >= c) {
+            System.out.println("B is largest");
+
+        } else {
+            System.out.println("C is largest");
+        }
+    }
 }
