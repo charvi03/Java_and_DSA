9ccdbdd9f9743a2f69e6c1ddb3a996de6dde6138
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-14 13:48:25 +0530

    init

---
+++ src/Conditionals/practice_q2.java
@@ -0,0 +1,14 @@
+package Conditionals;
+
+public class practice_q2 {
+    public static void main(String[] args) {
+        // You have a fever if your temperature
+        //is above 100 and otherwise prints You don't have a fever
+        double temp = 99.7;
+        if (temp > 100.0) {
+            System.out.println("You have fever");
+        } else {
+            System.out.println("You don't have fever");
+        }
+    }
+}
