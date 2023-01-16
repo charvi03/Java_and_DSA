10085b5831c2f2d9c304c82020604c659b69d3ba
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-16 22:04:41 +0530

    init

--- src/loopControl/reverse_given_number.java
+++ src/loopControl/reverse_given_number.java
@@ -1,4 +1,18 @@
 package loopControl;
 
+import java.util.Scanner;
+
 public class reverse_given_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        int rev = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            rev = (rev * 10) + lastDigit;
+            n = n / 10;
+        }
+        System.out.println(rev);
+    }
 }
