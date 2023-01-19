a88126fd3a7efc6d3803c82d79bc2e489d27cfa6
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-19 22:13:31 +0530

    init

---
+++ src/patterns/inverted_half_pyramid_numbers.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class inverted_half_pyramid_numbers {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
