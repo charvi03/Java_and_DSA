1116c0b236218643d0912b55248a9209f787484d
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-21 15:48:28 +0530

    init

--- src/Functions/prime_or_not.java
+++ src/Functions/prime_or_not.java
@@ -2,14 +2,19 @@ package Functions;
 
 public class prime_or_not {
     public static boolean isPrime(int n) {
-        boolean isPrime = true;
+        //boolean isPrime = true; // need not declare
+        if (n==2){
+            return true;
+        }
         for (int i = 2; i < n - 1; i++) {
             if (n % i == 0) {
-                isPrime = false;
-                break;
+                // isPrime = false;
+                // break; //or use return statement --return false
+             return false;
             }
         }
-        return isPrime;
+        //return isPrime; // return true
+        return true;
     }
 
     public static void main(String[] args) {
