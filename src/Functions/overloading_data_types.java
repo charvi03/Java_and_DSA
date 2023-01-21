19aaa65381505a704f81ffc87d593101ebc4a207
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-21 15:37:45 +0530

    init

--- src/Functions/overloading_data_types.java
+++ src/Functions/overloading_data_types.java
@@ -1,17 +1,17 @@
 package Functions;
 
-public class function_overloading {
-    //using params
+public class overloading_data_types {
+    //using data types
     public static int sum(int a, int b) {
         return a + b;
     }
 
-    public static int sum(int a, int b, int c) {
-        return a + b + c;
+    public static float sum(float a, float b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
         System.out.println("func 1 : " + sum(3, 5));
-        System.out.println("func 2 : " + sum(5, 2, 1));
+        System.out.println("func 2 : " + sum(3.2f, 4.8f));
     }
 }
