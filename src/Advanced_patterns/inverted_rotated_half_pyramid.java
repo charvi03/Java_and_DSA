41007d687481153c763e6c8020c4c4072faf74c9
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-22 16:45:03 +0530

    init

--- src/Advanced_patterns/inverted_rotated_half_pyramid.java
+++ src/Advanced_patterns/inverted_rotated_half_pyramid.java
@@ -1,7 +1,22 @@
 package Advanced_patterns;
 
 public class inverted_rotated_half_pyramid {
-    public static void main(String[] args) {
+    public static void printStars(int rows, int cols) {
+        int n = 4;
+        for (int i = 1; i <= rows; i++) {
+            //spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printStars(4, 4);
     }
 }
