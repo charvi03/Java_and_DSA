618e5823e4265a5900a2cdf1ff33eb670369314c
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-24 22:45:52 +0530

    init

--- src/Arrays/passing_arg.java
+++ src/Arrays/passing_arg.java
@@ -1,7 +1,19 @@
 package Arrays;
 
 public class passing_arg {
-    public static void main(String[] args) {
+    public static void update(int marks[]) {
+        for (int i = 0; i <marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
 
+    public static void main(String[] args) {
+        int marks[] = {98, 97, 99};
+        update(marks);
+        //print array
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+        System.out.println();
     }
 }
