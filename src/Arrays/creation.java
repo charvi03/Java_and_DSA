674a60dbf358511605c792f30de172a50e06fd8d
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-24 22:21:16 +0530

    init

---
+++ src/Arrays/creation.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class creation {
+    public static void main(String[] args) {
+        int marks[] = new int[50];//default zero
+        int numbers[] = {1, 2, 3, 4};
+        String fruits[] = {"apple", "banana", "cherry"};
+
+    }
+}
