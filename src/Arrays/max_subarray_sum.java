835725b29fc66bbeff3188d0aa5860b85d53d87a
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-01-26 21:22:13 +0530

    init

---
+++ src/Arrays/max_subarray_sum.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class max_subarray_sum {
+    //brute force--max subarray sum
+    public static void maxSubarrr(int num[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            int start = i;
+            for (int j = i; j < num.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k = start; k <= end; k++) {
+                    currSum += num[k];
+                }
+                System.out.println(currSum
+                );
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+
+            }
+
+        }
+        System.out.println("max sum = " + maxSum);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        maxSubarrr(numbers);
+
+    }
+}
