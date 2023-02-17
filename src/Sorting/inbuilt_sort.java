ed093a57f828157b88c7f4f80ad61a3f0143fd10
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-17 21:47:39 +0530

    init

---
+++ src/Sorting/inbuilt_sort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class inbuilt_sort {
+    public static void printArr(Integer arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer arr[] = {5, 4, 1, 3, 2};
+//        Arrays.sort(arr);
+//        printArr(arr);
+//        Arrays.sort(arr, 0, 3);
+//        printArr(arr);
+        //Arrays.sort(arr, Collections.reverseOrder());
+        //works on INTEGER (primitive ) NOT ON int
+        Arrays.sort(arr, 0, 3, Collections.reverseOrder());
+        printArr(arr);
+    }
+}
