a61501feebdba805cf7e011191db1478325b881e
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-18 21:48:14 +0530

    init

--- src/Two_d_arrays/creation_2d.java
+++ src/Two_d_arrays/creation_2d.java
@@ -3,6 +3,21 @@ package Two_d_arrays;
 import java.util.Scanner;
 
 public class creation_2d {
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("found at (" + i + "," + j + ")");
+                    return true;
+                }
+
+            }
+
+        }
+        System.out.println("not found");
+        return false;
+    }
+
     public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         // int n=3,m=3;
@@ -22,5 +37,6 @@ public class creation_2d {
             }
             System.out.println();
         }
+        search(matrix, 5);
     }
 }
