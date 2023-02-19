2327ca1b075f7586968fff4c49602c54795bbba4
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-19 15:05:05 +0530

    init

---
+++ src/StringsJava/comparison.java
@@ -0,0 +1,25 @@
+package StringsJava;
+
+public class comparison {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+        String s3 = new String("java");
+
+//        if (s1 == s2) {
+//            System.out.println("Strings are equal");
+//        } else {
+//            System.out.println("Strings are not equal");
+//        }
+//        if (s1 == s3) {
+//            System.out.println("Strings are equal");
+//        } else {
+//            System.out.println("Strings are not equal");
+//        }
+        if (s1.equals(s3)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
