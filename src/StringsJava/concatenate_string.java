76bb33bda1ccb040822a99aa2d789d287a871404
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-19 14:34:22 +0530

    init

--- src/StringsJava/concatenate_string.java
+++ src/StringsJava/concatenate_string.java
@@ -2,6 +2,9 @@ package StringsJava;
 
 public class concatenate_string {
     public static void main(String[] args) {
-
+        String firstName = "charvi";
+        String lastName = "khurana";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
     }
 }
