514e5a2420d8ba15668db5d87be2bbfe0c0e8895
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-19 18:52:19 +0530

    init

---
+++ src/Bit_Manipulation/binary_operators.java
@@ -0,0 +1,18 @@
+package Bit_Manipulation;
+
+public class binary_operators {
+    public static void main(String[] args) {
+        //bitwise and
+        System.out.println(5 & 6);
+        //bitwise or
+        System.out.println(5 | 6);
+        //bitwise xor
+        System.out.println(5 ^ 6);
+        //one's complement
+        System.out.println(~5);
+        //left shift
+        System.out.println(5<<2);
+        //right shift
+        System.out.println(6>>1);
+    }
+}
