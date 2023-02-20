6c11c6a91721d0ff4e67eda72fa24d52e206ebd8
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-20 18:05:22 +0530

    init

---
+++ src/OOPS/multi_level_inheritance.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+public class multi_level_inheritance {
+    public static void main(String[] args) {
+        Dog doggy = new Dog();
+        doggy.eat();
+        doggy.legs = 4;
+        System.out.println(doggy.legs );
+    }
+}
+
+class Animals {
+    String color;
+
+    void eat() {
+        System.out.println("eats");
+    }
+
+    void breathe() {
+        System.out.println("breathes");
+    }
+}
+
+class Mammals extends Animals {
+    int legs;
+}
+
+class Dog extends Mammals {
+    String breed;
+}
