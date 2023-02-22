18952eec9419676e7c03ee371163c3f2a1b9c042
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-22 17:01:31 +0530

    init

---
+++ src/Recursion/check_sorted.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class check_sorted {
+    public static boolean isSorted(int arr[], int i) {
+        if (i == arr.length - 1) {
+            return true;
+        }
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+        return isSorted(arr, i + 1);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 4, 5};
+        System.out.println(isSorted(arr, 0));
+
+
+    }
+}
