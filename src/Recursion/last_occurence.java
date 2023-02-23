1e0412cce02e9f54563b1be75aa99eb1399d4993
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-23 12:44:56 +0530

    init

---
+++ src/Recursion/last_occurence.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class last_occurence {
+    public static int last(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+
+        int isFound = last(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        System.out.println(last(arr, 5, 0));
+    }
+}
