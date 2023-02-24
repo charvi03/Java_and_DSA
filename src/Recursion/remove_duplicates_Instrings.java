58a6f140cf0d0b06c0aa05b235c618824ef3d0d1
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-24 20:26:36 +0530

    init

--- src/Recursion/remove_duplicates_Instrings.java
+++ src/Recursion/remove_duplicates_Instrings.java
@@ -1,7 +1,23 @@
 package Recursion;
 
 public class remove_duplicates_Instrings {
-    public static void main(String[] args) {
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        //work
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            removeDuplicate(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
 
+    public static void main(String[] args) {
+        String str = "appnnacollege";
+        removeDuplicate(str, 0, new StringBuilder(" "),new boolean[26]);
     }
 }
