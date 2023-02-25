02be85833a5cb4eee0206bc363cc7772db983091
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-25 15:36:40 +0530

    init

--- src/Recursion/binary_string.java
+++ src/Recursion/binary_string.java
@@ -1,17 +1,19 @@
 package Recursion;
 
 public class binary_string {
-    public static void printBinary(int n, int lastPlace, StringBuilder str) {
+    public static void printBinary(int n, int lastPlace, String str) {
         if (n == 0) {
             System.out.println(str);
+            return;
         }
-        printBinary(n - 1, 0, str.append("0"));
+        printBinary(n - 1, 0, str + "0");
         if (lastPlace == 0) {
-            printBinary(n - 1, 1, str.append("1"));
+            printBinary(n - 1, 1, str + "1");
         }
     }
 
     public static void main(String[] args) {
+        printBinary(3, 0, " ");
 
     }
 }
