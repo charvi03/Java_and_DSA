a2ff7850c6392a7fc3d1ba41056aadfc3a712bad
charvi03 <charvikhurana01@gmail.com>
2023-08-10 11:32:39 +0530

    init

--- src/Array_List/container_mostwater_2Pointer.java
+++ src/Array_List/container_mostwater_2Pointer.java
@@ -2,20 +2,30 @@ package Array_List;
 
 import java.util.ArrayList;
 
-public class Container_with_water {
+public class container_mostwater_2Pointer {
+    //2 pointer approach
     public static int storeWater(ArrayList<Integer> height) {
         int maxWater = 0;
-        //brute force
-        for (int i = 0; i < height.size(); i++) {
-            for (int j = i + 1; j < height.size(); j++) {
-                int ht = Math.min(height.get(i), height.get(j));
-                int width = j - i;
-                int currWater = ht * width;
-                maxWater = Math.max(maxWater, currWater);
+        int lp = 0;
+        int rp = height.size() - 1;
+        while (lp < rp) {
+            //calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //update pointer
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+
             }
         }
         return maxWater;
     }
+
 
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
